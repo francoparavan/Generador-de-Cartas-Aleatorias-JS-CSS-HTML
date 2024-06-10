ae65ae6e2b73bd96da481849b29f0b8efdf6ede7
Franco Paraván <francoparavan@gmail.com>
2024-06-10 00:41:09 +0000

    guardado para entrega

--- src/app.js
+++ src/app.js
@@ -2,10 +2,30 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  displayRandomCard();
+  document
+    .getElementById("randomizar-button")
+    .addEventListener("click", displayRandomCard);
+  setInterval(displayRandomCard, 10000);
 };
+
+function displayRandomCard() {
+  const cards = [
+    { value: "8", suit: "♠", color: "black" },
+    { value: "3", suit: "♥", color: "red" },
+    { value: "8", suit: "♥", color: "red" },
+    { value: "J", suit: "♣", color: "black" }
+  ];
+
+  const randomIndex = Math.floor(Math.random() * cards.length);
+  const card = cards[randomIndex];
+
+  document.getElementById("value").innerText = card.value;
+  document.getElementById("top-symbol").innerText = card.suit;
+  document.getElementById("bottom-symbol").innerText = card.suit;
+
+  document.getElementById("value").style.color = card.color;
+  document.getElementById("top-symbol").style.color = card.color;
+  document.getElementById("bottom-symbol").style.color = card.color;
+}
